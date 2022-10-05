31373e312a3d2cb36b2acb3771269cec836fbc91
ABISHEK-KS <97246536+ABISHEK-KS@users.noreply.github.com>
2022-10-05 21:10:40 +0530

    Add files via upload

---
+++ matplot_script.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+ 
+xax=[2014,2015,2016,2017,2018,2019,2020]
+yax=[7425842 ,7759681 ,8506426 ,9707959 ,10209178 ,10577407 ,2651536]
+plt.plot(xax,yax)
+plt.xlabel('YEAR')
+plt.ylabel('Influx in Crores')
+plt.grid()
+plt.show()
